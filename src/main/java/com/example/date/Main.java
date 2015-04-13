ab657619565cb6b0d6d1dea0f8488eae3bc8bc3e
rinat <rinat.zalyaletdinov@sunet.uz>
2015-04-13 17:58:28 +0500

    Date overlapping test

--- src/main/java/com/example/date/Main.java
+++ src/main/java/com/example/date/Main.java
@@ -2,7 +2,9 @@ package com.example.date;
 
 import org.apache.log4j.Logger;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.Calendar;
 
 /**
  * Created by Rinat Zalyaletdinov
@@ -12,8 +14,23 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.go();
+    }
+
+    private void go() {
+        dateOverlapping();
+    }
+
+    private void showLengthOfMonth() {
         YearMonth date = YearMonth.now();
         LOGGER.info(date.lengthOfMonth());
+    }
+
+    private void dateOverlapping() {
+        LocalDate date1 = LocalDate.of(2015, Calendar.APRIL, 13);
+        LocalDate date2 = LocalDate.of(2015, Calendar.APRIL, 14);
+        LOGGER.info(date1.isBefore(date2));
     }
 
 }
