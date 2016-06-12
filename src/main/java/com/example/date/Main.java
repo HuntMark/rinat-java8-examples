a6f1ba3727c389d1d197350ae50e3432f449efef
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-06-12 12:57:42 +0500

    recognize first day of week, last day of month and so on

--- src/main/java/com/example/date/Main.java
+++ src/main/java/com/example/date/Main.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
@@ -12,30 +14,40 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
 
-    private static final Logger LOGGER = Logger.getLogger(Main.class);
+  private static final Logger LOGGER = Logger.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        Main main = new Main();
-        main.go();
-    }
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.go();
+  }
 
-    private void go() {
-        timeUnitTest();
-    }
+  private void go() {
+    weekDays();
+    monthDays();
+  }
 
-    private void showLengthOfMonth() {
-        YearMonth date = YearMonth.now();
-        LOGGER.info(date.lengthOfMonth());
-    }
+  private void showLengthOfMonth() {
+    YearMonth date = YearMonth.now();
+    LOGGER.info(date.lengthOfMonth());
+  }
 
-    private void dateOverlapping() {
-        LocalDate date1 = LocalDate.of(2015, Calendar.APRIL, 13);
-        LocalDate date2 = LocalDate.of(2015, Calendar.APRIL, 14);
-        LOGGER.info(date1.isBefore(date2));
-    }
+  private void dateOverlapping() {
+    LocalDate date1 = LocalDate.of(2015, Calendar.APRIL, 13);
+    LocalDate date2 = LocalDate.of(2015, Calendar.APRIL, 14);
+    LOGGER.info(date1.isBefore(date2));
+  }
 
-    private void timeUnitTest() {
-        LOGGER.info(String.format("Minutes in hour: %d", TimeUnit.HOURS.toMinutes(1)));
-    }
+  private void timeUnitTest() {
+    LOGGER.info(String.format("Minutes in hour: %d", TimeUnit.HOURS.toMinutes(1)));
+  }
 
+  private void weekDays() {
+    LocalDate firstOfWeek = LocalDate.now().with(ChronoField.DAY_OF_MONTH, 1);
+    LOGGER.info(firstOfWeek + " : " + firstOfWeek.getDayOfWeek());
+  }
+
+  private void monthDays() {
+    LocalDate firstOfMonth = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+    LOGGER.info(firstOfMonth + " : " + firstOfMonth.getDayOfWeek());
+  }
 }
