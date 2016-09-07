ec9c0e3ebcb6318c3250583d962581b76a098aef
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-09-07 13:45:10 +0500

    timezone issue

--- src/main/java/com/example/msk_timezone_issue/Main.java
+++ src/main/java/com/example/msk_timezone_issue/Main.java
@@ -5,8 +5,10 @@ import java.time.ZoneId;
 
 public class Main {
   public static void main(String[] args) {
+    ZoneId moscowZoneId = ZoneId.of("Europe/Moscow");
+    System.out.println(moscowZoneId.getId());
     OffsetDateTime local = OffsetDateTime.now();
-    OffsetDateTime moscow = OffsetDateTime.now(ZoneId.of("Europe/Moscow"));
+    OffsetDateTime moscow = OffsetDateTime.now(moscowZoneId);
     System.out.println("Local time: " + local);
     System.out.println("Moscow time: " + moscow);
   }
