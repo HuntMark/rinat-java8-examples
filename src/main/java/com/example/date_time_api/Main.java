48522e1723b2a40f89b7b5d1bdda97dce7991306
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-09-22 16:43:04 +0500

    date format

--- src/main/java/com/example/date_time_api/Main.java
+++ src/main/java/com/example/date_time_api/Main.java
@@ -4,19 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.util.TimeZone;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    OffsetDateTime now = OffsetDateTime.now();
-    LOGGER.info(now.toString());
-    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
-    ZoneId zoneId = ZoneId.of("Europe/Moscow");
-    now = OffsetDateTime.of(now.toLocalDateTime(), zoneId.getRules().getOffset(LocalDateTime.now()));
-    LOGGER.info(now.toString());
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    String time = "2016-01-01 00:00";
+    LocalDateTime date = LocalDateTime.parse(time, formatter);
+    date = date.withSecond(0);
+    LOGGER.info(date.format(DateTimeFormatter.ISO_DATE_TIME));
   }
 }
