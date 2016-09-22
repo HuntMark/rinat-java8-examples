d39cacf054c258f692747b7661e45acc67f0c49b
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-09-22 11:43:27 +0500

    time zones

--- src/main/java/com/example/date_time_api/Main.java
+++ src/main/java/com/example/date_time_api/Main.java
@@ -3,13 +3,19 @@ package com.example.date_time_api;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println(Instant.now());
     System.out.println(LocalDateTime.now());
     System.out.println(DayOfWeek.TUESDAY.getDisplayName(TextStyle.NARROW, new Locale("ru")));
+    System.out.println(TimeZone.getDefault());
+    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+    System.out.println(OffsetDateTime.now());
+    System.out.println(TimeZone.getDefault());
   }
 }
