439f4b1f699e9cb7cbad8e985eec26df1e6092a8
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-12-06 08:46:31 +0300

    process collection WITH stream

--- src/main/java/com/example/oracle_stream_api_examples/Main.java
+++ src/main/java/com/example/oracle_stream_api_examples/Main.java
@@ -1,13 +1,16 @@
 package com.example.oracle_stream_api_examples;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Transaction> transactions = Arrays.asList(
                 new Transaction(1L, Transaction.Type.COMMUNAL, new BigDecimal(300)),
-                new Transaction(2L, Transaction.Type.GROCERY, new BigDecimal(100)),
+                new Transaction(2L, Transaction.Type.GROCERY, new BigDecimal(200)),
                 new Transaction(3L, Transaction.Type.GROCERY, new BigDecimal(150)),
                 new Transaction(4L, Transaction.Type.GROCERY, new BigDecimal(600)),
                 new Transaction(5L, Transaction.Type.COMMUNAL, new BigDecimal(200)));
@@ -16,25 +19,11 @@ public class Main {
     }
 
     public static List<Long> findTransactionIdsByType(List<Transaction> transactions, Transaction.Type type) {
-        List<Transaction> filtered = new ArrayList<>();
-        for (Transaction transaction : transactions) {
-            if (transaction.getType() == type) {
-                filtered.add(transaction);
-            }
-        }
-
-        Collections.sort(filtered, new Comparator<Transaction>() {
-            @Override
-            public int compare(Transaction t1, Transaction t2) {
-                return t1.getAmount().compareTo(t2.getAmount());
-            }
-        });
-
-        List<Long> ids = new ArrayList<>();
-        for (Transaction transaction : filtered) {
-            ids.add(transaction.getId());
-        }
-
-        return ids;
+        return transactions
+                .stream()
+                .filter((t) -> t.getType() == type)
+                .sorted(Comparator.comparing(Transaction::getAmount).reversed())
+                .map(Transaction::getId)
+                .collect(Collectors.toList());
     }
 }
