d189883730e7aeaee06f00a0019af273b2e3b990
HuntMark <rinat.zalyaletdinov@yandex.com>
2016-12-22 13:13:46 +0300

    groupingBy

--- src/main/java/com/example/oracle_stream_api_examples/Transaction.java
+++ src/main/java/com/example/oracle_stream_api_examples/Transaction.java
@@ -8,17 +8,27 @@ public class Transaction {
     }
 
     private Long id;
+    private String city;
     private Type type;
     private BigDecimal amount = new BigDecimal(0);
 
     public Transaction(Long id, Type type, BigDecimal amount) {
+        this(id, "Moscow", type, amount);
+    }
+
+    public Transaction(Long id, String city, Type type, BigDecimal amount) {
         this.id = id;
+        this.city = city;
         this.type = type;
         this.amount = amount;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getCity() {
+        return city;
     }
 
     public Type getType() {
@@ -33,6 +43,7 @@ public class Transaction {
     public String toString() {
         return "Transaction{" +
                 "id=" + id +
+                ", city='" + city + '\'' +
                 ", type=" + type +
                 ", amount=" + amount +
                 '}';
--- src/main/java/com/example/oracle_stream_api_examples/part2/Main.java
+++ src/main/java/com/example/oracle_stream_api_examples/part2/Main.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,17 +20,24 @@ import static java.util.stream.Collectors.groupingBy;
 public class Main {
     public static void main(String[] args) throws IOException {
         List<Transaction> transactions = Arrays.asList(
-                new Transaction(1L, Transaction.Type.COMMUNAL, new BigDecimal(300)),
-                new Transaction(2L, Transaction.Type.GROCERY, new BigDecimal(200)),
-                new Transaction(3L, Transaction.Type.GROCERY, new BigDecimal(150)),
-                new Transaction(4L, Transaction.Type.GROCERY, new BigDecimal(600)),
-                new Transaction(5L, Transaction.Type.COMMUNAL, new BigDecimal(200)));
+                new Transaction(1L, "Tashkent", Transaction.Type.COMMUNAL, new BigDecimal(300)),
+                new Transaction(2L, "Tashkent", Transaction.Type.GROCERY, new BigDecimal(200)),
+                new Transaction(3L, "Moscow", Transaction.Type.GROCERY, new BigDecimal(150)),
+                new Transaction(4L, "Moscow", Transaction.Type.GROCERY, new BigDecimal(600)),
+                new Transaction(5L, "Tashkent", Transaction.Type.COMMUNAL, new BigDecimal(200)));
 
         listing2();
         listing7();
         listing11(transactions);
         listing12(transactions);
         listing13(transactions);
+        listing14(transactions);
+        listing15(transactions);
+        listing17(transactions);
+        listing18(transactions);
+        listing19(transactions);
+        listing20(transactions);
+        listing21(transactions);
     }
 
     private static void listing2() {
@@ -78,5 +83,64 @@ public class Main {
                         .stream()
                         .collect(Collectors.averagingInt(tr -> tr.getAmount().intValue()));
         System.out.println("Average: " + average);
+    }
+
+    private static void listing14(List<Transaction> transactions) {
+        Optional<Transaction> highestTransaction =
+                transactions
+                        .stream()
+                        .collect(Collectors.maxBy(Comparator.comparing(tr -> tr.getAmount().intValue())));
+        System.out.println("The highest transaction: " + highestTransaction);
+    }
+
+    private static void listing15(List<Transaction> transactions) {
+        int totalValue =
+                transactions
+                        .stream()
+                        .collect(Collectors.reducing(0, tr -> tr.getAmount().intValue(), Integer::sum));
+        System.out.println("Total value: " + totalValue);
+    }
+
+    private static void listing17(List<Transaction> transactions) {
+        Map<Transaction.Type, List<Transaction>> transactionsByTypes =
+                transactions
+                        .stream()
+                        .collect(Collectors.groupingBy(Transaction::getType));
+        System.out.println(transactionsByTypes);
+    }
+
+    private static void listing18(List<Transaction> transactions) {
+        Map<Boolean, List<Transaction>> partitionedTransactions =
+                transactions
+                        .stream()
+                        .collect(Collectors.partitioningBy(t -> t.getAmount().compareTo(new BigDecimal(250)) > 0));
+        System.out.println(partitionedTransactions);
+    }
+
+    private static void listing19(List<Transaction> transactions) {
+        Map<Transaction.Type, Integer> cityToSum =
+                transactions.stream().collect(groupingBy(
+                        Transaction::getType, Collectors.summingInt(t -> t.getAmount().intValue())));
+        System.out.println(cityToSum);
+    }
+
+    private static void listing20(List<Transaction> transactions) {
+        Map<Transaction.Type, Optional<Transaction>> cityToHighestTransaction =
+                transactions.stream().collect(groupingBy(
+                        Transaction::getType, Collectors.maxBy(Comparator.comparing(t -> t.getId().intValue()))));
+        System.out.println(cityToHighestTransaction);
+    }
+
+    private static void listing21(List<Transaction> transactions) {
+        Map<String, Map<Transaction.Type, Double>> cityByCurrencyToAverage =
+                transactions
+                        .stream()
+                        .collect(
+                                groupingBy(
+                                        Transaction::getCity,
+                                        groupingBy(
+                                                Transaction::getType,
+                                                Collectors.averagingInt(t -> t.getAmount().intValue()))));
+        System.out.println(cityByCurrencyToAverage);
     }
 }
