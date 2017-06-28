e71944c178ea162c362f6a5a06f8c0ed620e1e22
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2017-06-28 15:20:44 +0300

    Stepik integrate

--- src/main/java/com/example/functional_interfaces/StepikIntegrate.java
+++ src/main/java/com/example/functional_interfaces/StepikIntegrate.java
@@ -5,9 +5,25 @@ import java.util.function.DoubleUnaryOperator;
 public class StepikIntegrate {
     public static void main(String[] args) {
         System.out.println(integrate(i -> 1, 0, 10));
+        System.out.println(integrate(x -> x + 2, 0, 10));
     }
 
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
-        return f.applyAsDouble(a);
+        int i;
+        double result, h;
+
+        result = 0;
+
+        int n = 1_000_000;
+
+        h = (b - a) / n;
+
+        for (i = 0; i < n; i++) {
+            result += f.applyAsDouble(a + h * (i + 0.5)); //Вычисляем в средней точке и добавляем в сумму
+        }
+
+        result *= h;
+
+        return result;
     }
 }
