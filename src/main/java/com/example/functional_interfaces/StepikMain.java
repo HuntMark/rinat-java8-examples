ef496a0c17fe54e2d93671fdbc22e36b8cd3444f
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2017-06-28 10:22:31 +0300

    Functional interfaces

--- src/main/java/com/example/functional_interfaces/StepikMain.java
+++ src/main/java/com/example/functional_interfaces/StepikMain.java
@@ -5,7 +5,13 @@ import java.math.BigInteger;
 public class StepikMain {
     public static void main(String[] args) {
         MyTimer timer = new MyTimer();
-        timer.measureTime(new RunnableImpl());
+
+        timer.measureTime(StepikMain::bigIntegerPower);
+    }
+
+    // extract method via ctrl + alt + M
+    private static BigInteger bigIntegerPower() {
+        return new BigInteger("12345").pow(10000);
     }
 }
 
@@ -14,12 +20,5 @@ class MyTimer {
         long startTime = System.currentTimeMillis();
         runnable.run();
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime));
-    }
-}
-
-class RunnableImpl implements Runnable {
-    @Override
-    public void run() {
-        new BigInteger("12345").pow(10000);
     }
 }
