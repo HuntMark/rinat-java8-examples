7e8145e9c091a33890009b6d1776c6a2c11c2ca4
HuntMark <rinat.zalyaletdinov@yandex.com>
2017-07-08 12:37:37 +0300

    Mod test

---
+++ src/main/java/com/example/stepik_java/ModTest.java
@@ -0,0 +1,8 @@
+package com.example.stepik_java;
+
+public class ModTest {
+    public static void main(String[] args) {
+        System.out.println(7 % 3);
+        System.out.println(7 % -3);
+    }
+}
